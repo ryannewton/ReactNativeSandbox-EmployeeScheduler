76138dd3c6f7caef11efd5109cda0f61245e3f31
Ryan Newton <newton1988@gmail.com>
2017-01-26 22:26:29 -0500

    Initilized firebase for authentication

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 //Import components, actions/reducers, and styles
 import reducers from './reducers';
+import firebaseConfig from '../api_keys';
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp(firebaseConfig);
+	}
+
 	render() {
 		return(
 			<Provider store={createStore(reducers)}>
