bbe57b13bfe0cb89dccda8a5047cab4c497e7969
Ryan Newton <newton1988@gmail.com>
2017-01-28 16:05:19 -0500

    React boilerplate for EmployeeList component

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Import Libraries
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Employee List</Text>
+			</View>
+		);
+	}
+}
+
+export default EmployeeList;
