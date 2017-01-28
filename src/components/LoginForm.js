b5636dd882412ae51a475b6e64d429de2662ae38
Ryan Newton <newton1988@gmail.com>
2017-01-28 11:10:32 -0500

    Updates state when user inputs email and password

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,11 +2,21 @@
 
 // Import libraries
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 //Import components, actions/reducers, and styles
 import { Card, CardSection, Input, Button } from './common';
+import { emailChanged, passwordChanged } from '../actions';
 
 class LoginForm extends Component {
+	onEmailChange(text) {
+		this.props.emailChanged(text);
+	}
+
+	onPasswordChange(text) {
+		this.props.passwordChanged(text);
+	}
+
 	render() {
 		return (
 			<Card>
@@ -14,6 +24,8 @@ class LoginForm extends Component {
 					<Input
 						label="Email"
 						placeholder="joe@gmail.com"
+						value={this.props.email}
+						onChangeText={this.onEmailChange.bind(this)}
 					/>
 				</CardSection>
 
@@ -22,6 +34,8 @@ class LoginForm extends Component {
 						secureTextEntry
 						label="Password"
 						placeholder="password"
+						value={this.props.password}
+						onChangeText={this.onPasswordChange.bind(this)}
 					/>
 				</CardSection>
 
@@ -35,4 +49,9 @@ class LoginForm extends Component {
 	}
 }
 
-export default LoginForm;
+const mapStateToProps = (state) => {
+	const { email, password } = state.auth;
+	return { email, password };
+}
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
