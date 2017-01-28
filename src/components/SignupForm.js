095ed3bc72b39efce2b760c7559be78f4f3c389f
Ryan Newton <newton1988@gmail.com>
2017-01-28 12:46:59 -0500

    Adds user input components to SignupForm

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -2,13 +2,47 @@
 
 // Import Libraries
 import React, { Component } from 'react';
-import { Text } from 'react-native';
 import { connect } from 'react-redux';
+
+//Import components, actions/reducers, and styles
+import { Card, CardSection, Input, Button } from './common';
 
 class SignupForm extends Component {
 	render() {
 		return (
-			<Text>Test</Text>
+			<Card>
+				<CardSection>
+					<Input
+						label="Email"
+						placeholder="joe@gmail.com"
+						value={this.props.email}
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Input
+						secureTextEntry
+						label="Password"
+						placeholder="password"
+						value={this.props.password}
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Input
+						secureTextEntry
+						label="Confirm Password"
+						placeholder="password"
+						value={this.props.passwordConfirm}
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Sign Up
+					</Button>
+				</CardSection>
+			</Card>
 		);
 	}
 }
