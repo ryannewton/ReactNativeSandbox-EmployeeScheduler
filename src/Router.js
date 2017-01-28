6b31f598e99901a9ad229464b4fb5b01b203e833
Ryan Newton <newton1988@gmail.com>
2017-01-28 16:11:30 -0500

    Adds EmployeeList component to router

--- src/Router.js
+++ src/Router.js
@@ -2,17 +2,31 @@
 
 // Import libraries
 import React from 'react';
+import { Platform } from 'react-native';
 import { Scene, Router } from 'react-native-router-flux';
 
 //Import components, actions/reducers, and styles
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
 	return (
-		<Router>
-			<Scene key="login" component={LoginForm} title="Please Login" />
+		<Router sceneStyle={styles.sceneStyle}>
+			<Scene key="auth">
+				<Scene key="login" component={LoginForm} title="Please Login" />
+			</Scene>
+
+			<Scene key="main">
+				<Scene key="employeeList" component={EmployeeList} title="Employees" />
+			</Scene>
 		</Router>
 	);
+};
+
+const styles = {
+	sceneStyle: {
+		paddingTop: Platform.OS === 'android' ? 50 : 65
+	}
 };
 
 export default RouterComponent;
