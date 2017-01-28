7f1cf329073a63f34941cd2fe3cd9d21a61de157
Ryan Newton <newton1988@gmail.com>
2017-01-28 12:16:57 -0500

    Adds reducer for successful login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
+// Import libraries
+import firebase from 'firebase';
+
 import {
 	EMAIL_CHANGED,
-	PASSWORD_CHANGED
+	PASSWORD_CHANGED,
+	LOGIN_USER_SUCCESS
 } from './types';
 
 export const emailChanged = (text) => {
@@ -16,5 +20,14 @@ export const passwordChanged = (password) => {
 	return {
 		type: PASSWORD_CHANGED,
 		payload: password
+	};
+};
+
+export const loginUser = ({ email, password }) => {
+	return (dispatch) => {
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then((user) => {
+				dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+			});
 	};
 };
