b779444c0986fba6e2d6ffb59495f258be5cc840
Ryan Newton <newton1988@gmail.com>
2017-01-27 19:53:09 -0500

    Adds structure to LoginForm component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,12 +4,32 @@
 import React, { Component } from 'react';
 
 //Import components, actions/reducers, and styles
-import { Card } from './common';
+import { Card, CardSection, Input, Button } from './common';
 
 class LoginForm extends Component {
 	render() {
 		return (
 			<Card>
+				<CardSection>
+					<Input
+						label="Email"
+						placeholder="joe@gmail.com"
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Input
+						secureTextEntry
+						label="Password"
+						placeholder="password"
+					/>
+				</CardSection>
+
+				<CardSection>
+					<Button>
+						Login
+					</Button>
+				</CardSection>
 			</Card>
 		);
 	}
