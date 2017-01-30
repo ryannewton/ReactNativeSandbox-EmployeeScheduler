6fd344100e6f46f946a36bef6185b6ba330fa916
Ryan Newton <newton1988@gmail.com>
2017-01-29 20:38:12 -0500

    Add action creator to create an employee

---
+++ src/actions/EmployeeActions.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import {
+	EMPLOYEE_UPDATE
+} from './types';
+
+export const employeeUpdate = ({ prop, value }) => {
+	return {
+		type: EMPLOYEE_UPDATE,
+		payload: { prop, value }
+	};
+};
--- src/actions/types.js
+++ src/actions/types.js
@@ -9,3 +9,5 @@ export const SIGNUP_USER_SUCCESS = 'signup_user_success';
 export const SIGNUP_USER_FAIL = 'signup_user_fail';
 export const LOGIN_USER = 'login_user';
 export const SIGNUP_USER = 'signup_user';
+
+export const EMPLOYEE_UPDATE = 'employee_update';
