c30225757960c25e0f42b397219e8923a1db4edc
Ryan Newton <newton1988@gmail.com>
2017-01-29 23:55:07 -0500

    Reducer boilerplate for EmployeeFormReducer

---
+++ src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import {
+	EMPLOYEE_UPDATE
+} from '../actions/types';
+
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
