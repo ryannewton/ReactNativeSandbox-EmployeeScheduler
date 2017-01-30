12e94552d015406415fbd5dfcc26c542a290802d
Ryan Newton <newton1988@gmail.com>
2017-01-29 20:17:55 -0500

    React boilerplate for EmployeeCreate component

---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Import libraries
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeCreate extends Component {
+	render() {
+		return (
+			<View>
+				<Text>Employee Form</Text>
+			</View>
+		);
+	}
+}
+
+export default EmployeeCreate;
