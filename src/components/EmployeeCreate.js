5b048e39650f5efcfccc5ed263068ca27ab3c015
Ryan Newton <newton1988@gmail.com>
2017-01-29 20:30:03 -0500

    Add high-level structure to EmployeeCreate component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -4,12 +4,36 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+// Import components
+import { Card, CardSection, Input, Button } from './common';
+
 class EmployeeCreate extends Component {
 	render() {
 		return (
-			<View>
-				<Text>Employee Form</Text>
-			</View>
+			<Card>
+				<CardSection>
+					<Input
+						label="Name"
+						placeholder="Jane"
+					/>
+				</CardSection>
+				
+				<CardSection>
+					<Input
+						label="phone"
+						placeholder="555-555-5555"
+					/>
+				</CardSection>
+				
+				<CardSection>
+				</CardSection>
+				
+				<CardSection>
+					<Button>
+						Create
+					</Button>
+				</CardSection>
+			</Card>
 		);
 	}
 }
