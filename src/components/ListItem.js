e0c33e4171a0a2291bd9d53e68e53bb34f4460c4
Ryan Newton <newton1988@gmail.com>
2017-02-07 21:58:10 -0500

    Adds ListItem component for rendering a single employee on EmployeeList

---
+++ src/components/ListItem.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Import Libraries
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+// Import Components
+import { CardSection } from './common';
+
+class ListItem extends Component {
+	render() {
+		const { name } = this.props.employee;
+
+		return (
+			<CardSection>
+				<Text style={styles.titleStyle}>
+					{name}
+				</Text>
+			</CardSection>
+		);
+	}
+}
+
+const styles = {
+	titleStyle: {
+		fontSize: 18,
+		paddingLeft: 15
+	}
+};
+
+export default ListItem;
