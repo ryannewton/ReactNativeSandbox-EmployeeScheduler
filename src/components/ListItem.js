c52b683a902b7521b01a41b84f151d9cf6f67841
Ryan Newton <newton1988@gmail.com>
2017-02-08 22:10:01 -0500

    Makes ListItems in EmployeeList clickable, routing to EmployeeCreate component

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,21 +2,30 @@
 
 // Import Libraries
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 // Import Components
 import { CardSection } from './common';
 
 class ListItem extends Component {
+	onRowPress() {
+		Actions.employeeCreate({ employee: this.props.employee });
+	}
+
 	render() {
 		const { name } = this.props.employee;
 
 		return (
-			<CardSection>
-				<Text style={styles.titleStyle}>
-					{name}
-				</Text>
-			</CardSection>
+			<TouchableWithoutFeedback onPress={this.onRowPress.bind(this)}>
+				<View>
+					<CardSection>
+						<Text style={styles.titleStyle}>
+							{name}
+						</Text>
+					</CardSection>
+				</View>
+			</TouchableWithoutFeedback>
 		);
 	}
 }
