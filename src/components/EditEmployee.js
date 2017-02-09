8f2c580dea6d69df9024392647ab56e7d6d9a32c
Ryan Newton <newton1988@gmail.com>
2017-02-08 22:27:42 -0500

    Boilerplate for EmployeeEdit component

---
+++ src/components/EditEmployee.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Import Libraries
+import React, { Component } from 'react';
+
+// Import components
+import EmployeeForm from './EmployeeForm';
+import { Card, CardSection, Button } from './common';
+
+class EmployeeEdit extends Component {
+	render() {
+		return (
+			<Card>Placeholder</Card>
+		);
+	}
+}
+
+export default EmployeeEdit;
