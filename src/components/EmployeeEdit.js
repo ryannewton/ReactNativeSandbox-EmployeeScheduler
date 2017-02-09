d3745966d6e5c55c28c88be38bbcefab1fe53eb2
Ryan Newton <newton1988@gmail.com>
2017-02-08 23:11:12 -0500

    Prepopulates EmployeeEdit with details of the selected employee

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,20 +1,33 @@
 'use strict';
 
 // Import Libraries
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 // Import components
 import EmployeeForm from './EmployeeForm';
 import { Card, CardSection, Button } from './common';
+import { employeeUpdate } from '../actions';
 
 class EmployeeEdit extends Component {
+	componentWillMount() {
+		_.each(this.props.employee, (value, prop) => {
+			this.props.employeeUpdate({ prop, value });
+		});
+	}
+
+	onButtonPress() {
+		const { name, phone, shift } = this.props;
+		// To do: Update firebase DB with new data
+	}
+
 	render() {
 		return (
 			<Card>
 				<EmployeeForm />
 				<CardSection>
-					<Button>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Save Changes
 					</Button>
 				</CardSection>
@@ -23,4 +36,10 @@ class EmployeeEdit extends Component {
 	}
 }
 
-export default connect()(EmployeeEdit);
+const mapStateToProps = (state) => {
+	const { name, phone, shift } = state.employeeForm;
+
+	return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
