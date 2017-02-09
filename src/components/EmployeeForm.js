40d81eabdffd687e4f7a8341488f171bc076d38a
Ryan Newton <newton1988@gmail.com>
2017-02-08 22:10:56 -0500

    React boilerplate for EmployeeForm, a compnent to render employee details

---
+++ src/components/EmployeeForm.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Import Libraries
+import React, { Component } from 'react';
+
+class EmployeeForm extends Component {
+	render() {
+		return (
+			<div>filler</div>
+		);
+	}
+}
+
+export default EmployeeForm;
