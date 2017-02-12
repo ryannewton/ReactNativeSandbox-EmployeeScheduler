979e6aa08930742f436714e0b549d325373a1ce2
Ryan Newton <newton1988@gmail.com>
2017-02-12 02:49:16 -0500

    Adds action creator and reducer for saving an update to DB

--- src/actions/types.js
+++ src/actions/types.js
@@ -13,3 +13,4 @@ export const SIGNUP_USER = 'signup_user';
 export const EMPLOYEE_UPDATE = 'employee_update';
 export const EMPLOYEE_CREATE = 'employee_create';
 export const EMPLOYEES_FETCH_SUCCESS = 'employees_fetch_success';
+export const EMPLOYEE_SAVE_SUCCESS = 'employee_save_success';
